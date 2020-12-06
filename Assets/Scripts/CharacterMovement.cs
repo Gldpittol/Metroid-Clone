971e21540088d32583fde85bee58ccb213dbe0d7
Gabriel Pittol <gabrielpittol@hotmail.com>
2020-12-06 15:54:13 -0300

    character movement basics

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float playerSpeed;
+    public float jumpForce;
+    private Rigidbody2D rb;
+    public GroundCheck groundCheck;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && groundCheck.canJump)
+        {
+            Vector2 jumpVector = new Vector2(0f, jumpForce);
+            rb.AddForce(jumpVector, ForceMode2D.Impulse);
+        }
+        float velY = rb.velocity.y;
+
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * playerSpeed, velY);
+    }
+}
---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public bool canJump = false;
+    public GameObject player;
+
+    private void Update()
+    {
+        transform.position = player.transform.position;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            canJump = false;
+        }
+    }
+}
