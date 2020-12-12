7710471cc481e81bd2b34372ea25f8485e65fe63
Gabriel Pittol <gabrielpittol@hotmail.com>
2020-12-12 19:58:44 -0300

    fixes Zoomer

--- Assets/Scripts/ZoomerScript.cs
+++ Assets/Scripts/ZoomerScript.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class ZoomerScript : MonoBehaviour
 {
+    [TextArea(3,9)]
+    public string ZoomerTutorial = "Ângulos para usar em ''Rotations'', cada rotação para seu respectivo Target: \n0: Agarrado na parte de cima\n90:Agarrado na parede da esquerda\n180:Agarrado na parte de baixo\n270: Agarrado na parede da direita";
     public float speed = 10;
     private int i = 0;
 
     public GameObject[] Target;
+    public float[] Rotations;
     public GameObject currentTarget;
 
-    private void Awake()
+    public bool isCyclic;
+    private int direction;
+
+
+    private void Start()
     {
+        direction = 1;
         currentTarget = Target[i];
         GameObject.FindGameObjectWithTag("TargetZoomer").transform.SetParent(TargetGameObject.instance.gameObject.transform);
     }
@@ -22,8 +30,9 @@ public class ZoomerScript : MonoBehaviour
 
         if(transform.position == Target[i].transform.position)
         {
+            if(direction > 0) RotateEnemy();
             FindNewTarget();
-            RotateEnemy();
+            if(direction < 0) RotateEnemy();
         }
     }
 
@@ -34,29 +43,35 @@ public class ZoomerScript : MonoBehaviour
 
     public void FindNewTarget()
     {
-        i++;
-        if (i >= Target.Length)
-            i = 0;
-        currentTarget = Target[i];
+        print("oi");
+        if(isCyclic)
+        {
+            i += 1;
+            if (i >= Target.Length)
+                i = 0;
+            currentTarget = Target[i];
+        }
+
+        if(!isCyclic)
+        {
+            i += direction;
+            if (i >= Target.Length || i < 0)
+            {
+                direction *= -1;
+                i += direction * 2;
+                currentTarget = Target[i];
+            }
+            else
+            {
+                currentTarget = Target[i];
+            }
+        }
+
+
     }
 
     public void RotateEnemy()
     {
-        if(Target[i].transform.position.x > transform.position.x)
-        {
-            transform.rotation = Quaternion.Euler(0f, 0f, 270f);
-        }
-        else if(Target[i].transform.position.x < transform.position.x)
-        {
-            transform.rotation = Quaternion.Euler(0f, 0f, 90f);
-        }
-        if (Target[i].transform.position.y > transform.position.y)
-        {
-            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        }
-        else if (Target[i].transform.position.y < transform.position.y)
-        {
-            transform.rotation = Quaternion.Euler(0f, 0f, 180f);
-        }
+        transform.rotation = Quaternion.Euler(0f, 0f, Rotations[i]);
     }
 }
